a9d630cfced37f6c2923bf935611fdcdaeaaf7e0
phrtynus <fondantcn@gmail.com>
2025-04-01 18:43:29 +0800

    1

--- logger.go
+++ logger.go
@@ -5,155 +5,283 @@ import (
 	"io"
 	"log"
 	"os"
+	"path/filepath"
 	"sync"
 	"time"
 )
 
 const (
-	INFO  = "INFO] "
-	DEBUG = "DEBUG] "
-	WARN  = "WARN] "
-	ERROR = "ERROR] "
+	INFO  = 0
+	DEBUG = 1
+	WARN  = 2
+	ERROR = 3
 )
 
-type RotatingFile struct {
+var levelNames = []string{
+	"INFO",
+	"DEBUG",
+	"WARN",
+	"ERROR",
+}
+
+type LogConfig struct {
+	Filename       string
+	MaxSize        int64 // bytes
+	BufferSize     int   // lines
+	FlushInterval  time.Duration
+	StdoutLevels   map[int]bool
+	FileWriterOnly bool
+}
+
+type RotatingWriter struct {
+	logConfig  LogConfig
 	file       *os.File
-	maxSize    int64
 	currentLen int64
-	baseName   string
-	mu         sync.Mutex
+	rotateCh   chan struct{}
+	mu         sync.RWMutex
 }
 
 type Logger struct {
-	rf          *RotatingFile
-	errorLogger *log.Logger
-	warnLogger  *log.Logger
-	debugLogger *log.Logger
-	infoLogger  *log.Logger
-	panicLogger *log.Logger
+	writer    *RotatingWriter
+	logChan   chan *logEntry
+	closeCh   chan struct{}
+	wg        sync.WaitGroup
+	stdoutMap map[int]bool
 }
 
-func NewRotatingFile(filename string, maxSize int64) (*RotatingFile, error) {
-	f, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+type logEntry struct {
+	level  int
+	format string
+	args   []interface{}
+}
+
+func LoggerNew(logConfig LogConfig) (*Logger, error) {
+	rw := &RotatingWriter{
+		logConfig: logConfig,
+		rotateCh:  make(chan struct{}, 1),
+	}
+
+	if !logConfig.FileWriterOnly {
+		if err := rw.initializeFile(); err != nil {
+			return nil, err
+		}
+	}
+
+	l := &Logger{
+		writer:    rw,
+		logChan:   make(chan *logEntry, logConfig.BufferSize),
+		closeCh:   make(chan struct{}),
+		stdoutMap: make(map[int]bool),
+	}
+
+	for level := range logConfig.StdoutLevels {
+		l.stdoutMap[level] = true
+	}
+
+	l.wg.Add(1)
+	go l.processLogs()
+
+	if logConfig.FlushInterval > 0 {
+		l.wg.Add(1)
+		go l.autoFlush()
+	}
+
+	return l, nil
+}
+
+func (rw *RotatingWriter) initializeFile() error {
+	dir := filepath.Dir(rw.logConfig.Filename)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return fmt.Errorf("create log directory failed: %w", err)
+	}
+
+	f, err := os.OpenFile(rw.logConfig.Filename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
 	if err != nil {
-		return nil, err
+		return fmt.Errorf("open log file failed: %w", err)
 	}
 
 	stat, err := f.Stat()
 	if err != nil {
-		return nil, err
+		f.Close()
+		return fmt.Errorf("get file stats failed: %w", err)
 	}
 
-	return &RotatingFile{
-		file:       f,
-		maxSize:    maxSize,
-		currentLen: stat.Size(),
-		baseName:   filename,
-	}, nil
-}
-
-func (rf *RotatingFile) Write(p []byte) (n int, err error) {
-	rf.mu.Lock()
-	defer rf.mu.Unlock()
-
-	if rf.currentLen+int64(len(p)) >= rf.maxSize {
-		if err := rf.rotate(); err != nil {
-			return 0, err
-		}
-	}
-
-	n, err = rf.file.Write(p)
-	rf.currentLen += int64(n)
-	return n, err
-}
-
-func (rf *RotatingFile) rotate() error {
-	if rf.file != nil {
-		rf.file.Close()
-	}
-
-	i := 1
-	for {
-		newName := fmt.Sprintf("%s.%d", rf.baseName, i)
-		if _, err := os.Stat(newName); os.IsNotExist(err) {
-			if err := os.Rename(rf.baseName, newName); err != nil {
-				return err
-			}
-			break
-		}
-		i++
-	}
-
-	f, err := os.OpenFile(rf.baseName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
-	if err != nil {
-		return err
-	}
-
-	rf.file = f
-	rf.currentLen = 0
+	rw.file = f
+	rw.currentLen = stat.Size()
 	return nil
 }
 
-func NewLogger(filename string, maxSize int64) (*Logger, error) {
-	rf, err := NewRotatingFile(filename, maxSize)
-	if err != nil {
-		return nil, err
+func (rw *RotatingWriter) Write(p []byte) (int, error) {
+	rw.mu.RLock()
+	defer rw.mu.RUnlock()
+
+	if rw.file == nil {
+		return 0, fmt.Errorf("log file not initialized")
 	}
 
-	multiWriter := io.MultiWriter(rf, os.Stderr)
+	if rw.currentLen+int64(len(p)) > rw.logConfig.MaxSize {
+		select {
+		case rw.rotateCh <- struct{}{}:
+			go rw.safeRotate()
+		default:
+			// Rotation already in progress
+		}
+	}
 
-	return &Logger{
-		rf:          rf,
-		errorLogger: log.New(multiWriter, "", 0),
-		warnLogger:  log.New(multiWriter, "", 0),
-		debugLogger: log.New(multiWriter, "", 0),
-		infoLogger:  log.New(rf, "", 0),
-		panicLogger: log.New(multiWriter, "", 0),
-	}, nil
+	n, err := rw.file.Write(p)
+	rw.currentLen += int64(n)
+	return n, err
 }
 
-func (l *Logger) formatHeader(level string) string {
+func (rw *RotatingWriter) safeRotate() {
+	rw.mu.Lock()
+	defer rw.mu.Unlock()
+
+	if rw.file == nil {
+		return
+	}
+
+	now := time.Now().UnixNano()
+	newName := fmt.Sprintf("%s.%d", rw.logConfig.Filename, now)
+	if err := os.Rename(rw.logConfig.Filename, newName); err != nil {
+		log.Printf("log rotate failed: %v", err)
+		return
+	}
+
+	if err := rw.initializeFile(); err != nil {
+		log.Printf("reopen log file failed: %v", err)
+	}
+}
+
+func (l *Logger) processLogs() {
+	defer l.wg.Done()
+
+	var stdout io.Writer = os.Stderr
+	if l.writer.logConfig.FileWriterOnly {
+		stdout = io.Discard
+	}
+
+	for {
+		select {
+		case entry := <-l.logChan:
+			l.writeLog(entry, stdout)
+		case <-l.closeCh:
+			// Flush remaining logs
+			for {
+				select {
+				case entry := <-l.logChan:
+					l.writeLog(entry, stdout)
+				default:
+					return
+				}
+			}
+		}
+	}
+}
+
+func (l *Logger) writeLog(entry *logEntry, stdout io.Writer) {
+	msg := l.formatMessage(entry)
+	if _, ok := l.stdoutMap[entry.level]; ok {
+		fmt.Fprintln(stdout, msg) // 更快的标准输出
+	}
+
+	if l.writer.logConfig.FileWriterOnly {
+		return
+	}
+
+	if _, err := l.writer.Write([]byte(msg + "\n")); err != nil {
+		log.Printf("log write failed: %v", err)
+	}
+	defer func() {
+		recover()
+	}()
+	if entry.level == ERROR {
+		panic(msg)
+	}
+}
+
+func (l *Logger) formatMessage(entry *logEntry) string {
 	now := time.Now()
-	return fmt.Sprintf("[PHRYNUS][%s %s][%s",
+	var msg string
+
+	if entry.format == "" {
+		msg = fmt.Sprint(entry.args...)
+	} else {
+		msg = fmt.Sprintf(entry.format, entry.args...)
+	}
+
+	return fmt.Sprintf("[PHRYNUS][%s %s][%s] %s",
 		now.Format("2006/01/02"),
 		now.Format("15:04:05.000000"),
-		level)
+		levelNames[entry.level],
+		msg)
 }
 
-func (l *Logger) log(logger *log.Logger, level string, v ...interface{}) {
-	msg := fmt.Sprint(v...)
-	logger = log.New(logger.Writer(), l.formatHeader(level), 0)
-	logger.Output(3, msg)
+func (l *Logger) autoFlush() {
+	defer l.wg.Done()
+
+	ticker := time.NewTicker(l.writer.logConfig.FlushInterval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			l.Flush()
+		case <-l.closeCh:
+			return
+		}
+	}
 }
 
-func (l *Logger) logf(logger *log.Logger, level string, format string, v ...interface{}) {
-	msg := fmt.Sprintf(format, v...)
-	logger = log.New(logger.Writer(), l.formatHeader(level), 0)
-	logger.Output(3, msg)
+// Public Methods
+func (l *Logger) Log(level int, args ...interface{}) {
+	l.log(level, "", args...)
 }
 
-// 常规日志方法
-func (l *Logger) Info(v ...interface{})  { l.log(l.infoLogger, INFO, v...) }
-func (l *Logger) Debug(v ...interface{}) { l.log(l.debugLogger, DEBUG, v...) }
-func (l *Logger) Warn(v ...interface{})  { l.log(l.warnLogger, WARN, v...) }
-func (l *Logger) Error(v ...interface{}) {
-	s := fmt.Sprint(v...)
-	l.log(l.panicLogger, ERROR, s)
-	defer func() {
-		recover()
-	}()
-	panic(s)
+func (l *Logger) Logf(level int, format string, args ...interface{}) {
+	l.log(level, format, args...)
 }
 
-// 格式化日志方法
-func (l *Logger) Infof(format string, v ...interface{})  { l.logf(l.infoLogger, INFO, format, v...) }
-func (l *Logger) Debugf(format string, v ...interface{}) { l.logf(l.debugLogger, DEBUG, format, v...) }
-func (l *Logger) Warnf(format string, v ...interface{})  { l.logf(l.warnLogger, WARN, format, v...) }
-func (l *Logger) Errorf(format string, v ...interface{}) {
-	s := fmt.Sprintf(format, v...)
-	l.logf(l.panicLogger, ERROR, "%s", s)
-	defer func() {
-		recover()
-	}()
-	panic(s)
+func (l *Logger) Debug(args ...interface{}) { l.log(DEBUG, "", args...) }
+func (l *Logger) Info(args ...interface{})  { l.log(INFO, "", args...) }
+func (l *Logger) Warn(args ...interface{})  { l.log(WARN, "", args...) }
+func (l *Logger) Error(args ...interface{}) { l.log(ERROR, "", args...) }
+
+// func (l *Logger) Panic(args ...interface{}) { l.log(PANIC, "", args...) }
+
+func (l *Logger) Debugf(format string, args ...interface{}) { l.log(DEBUG, format, args...) }
+func (l *Logger) Infof(format string, args ...interface{})  { l.log(INFO, format, args...) }
+func (l *Logger) Warnf(format string, args ...interface{})  { l.log(WARN, format, args...) }
+func (l *Logger) Errorf(format string, args ...interface{}) { l.log(ERROR, format, args...) }
+
+// func (l *Logger) Panicf(format string, args ...interface{}) { l.log(PANIC, format, args...) }
+
+func (l *Logger) log(level int, format string, args ...interface{}) {
+	entry := &logEntry{
+		level:  level,
+		format: format,
+		args:   args,
+	}
+
+	select {
+	case l.logChan <- entry:
+	default:
+		// 缓冲满时直接写入避免阻塞
+		l.writeLog(entry, os.Stderr)
+	}
+}
+
+func (l *Logger) Flush() {
+	for len(l.logChan) > 0 {
+		time.Sleep(5 * time.Millisecond)
+	}
+}
+
+func (l *Logger) Close() {
+	close(l.closeCh)
+	l.wg.Wait()
+
+	if l.writer.file != nil {
+		l.writer.file.Close()
+	}
 }
--- main.go
+++ main.go
@@ -1,18 +1,36 @@
 package main
 
+import (
+	"time"
+)
+
 var (
 	logger *Logger
 	err    error = nil
 )
 
 func init() {
-	logger, err = NewLogger("main.log", 10*1024*1024) // 10MB分割
+	logger, err = LoggerNew(LogConfig{
+		Filename:      "main.log",
+		MaxSize:       10 * 1024 * 1024, // 10MB
+		BufferSize:    1000,
+		FlushInterval: 5 * time.Second,
+		StdoutLevels: map[int]bool{
+			1: true,
+			2: true,
+			3: true,
+		},
+	})
 	if err != nil {
 		panic(err)
 	}
 }
 func main() {
-	for i := 0; i < 1000000; i++ {
-		logger.Info("GO")
-	}
+	defer logger.Close() // 关闭日志文件
+	// 常规日志
+	logger.Info("System initialized")
+	logger.Debug("Cache hits:", 2450, " misses:", 12)
+	logger.Warnf("Connection attempt %d failed", 3)
+	logger.Error("Database connection lost")
+
 }
